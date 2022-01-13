fef3b66a0239015d21961743f73ac1d03ed736a2
aaronsingh00 <97674025+aaronsingh00@users.noreply.github.com>
2022-01-13 11:57:45 +0000

    Add files via upload

---
+++ Jenkins.java
@@ -0,0 +1,7 @@
+public class Jenkins {
+  public static void main(String[] args) {
+    System.out.println("Name: Aaron");
+    System.out.println("Surname: Chana");
+    System.out.println("Job Role: Student Technical");
+  }
+}
